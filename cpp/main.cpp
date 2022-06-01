7a5dc59a37088eb097f5b2214ee5a9e95980704a
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2022-06-01 11:26:25 +0100

    cpp: Add more comprehensive example
    As well as versions with frame pointers

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,27 +1,37 @@
+#include <fcntl.h>
 #include <iostream>
+#include <unistd.h>
 
-unsigned int fibonacci(unsigned int n)
-{
-    if (n == 0)
-        return 0;
 
-    unsigned int a = 1, b = 1;
-    for (unsigned int i = 3; i <= n; ++i)
-    {
-        unsigned int fib = a + b;
-        a = b;
-        b = fib;
-    }
+int __attribute__((noinline)) top() {
+  for (int i = 0; i < 100000; i++) {
+    int fd = open("/", O_DIRECTORY);
+    close(fd);
+  }
 
-    return b;
+  return 0;
 }
 
-int main()
-{
-    int limit = 1000000;
-    for (int i = 0; i < limit; i++)
-    {
-        std::cout << "F(" << i << ") = " << fibonacci(i) << std::endl;
-    }
-    return 0;
+// ones
+int __attribute__((noinline)) c1() { return top(); }
+
+int __attribute__((noinline)) b1() { return c1(); }
+
+int __attribute__((noinline)) a1() { return b1(); }
+
+// twos
+int __attribute__((noinline)) c2() { return top(); }
+
+int __attribute__((noinline)) b2() { return c2(); }
+
+int __attribute__((noinline)) a2() { return b2(); }
+
+int main() {
+  while (true) {
+    std::cout << "Calling a1" << std::endl;
+    a1();
+    std::cout << "Calling a2" << std::endl;
+    a2();
+  }
+  return 0;
 }
