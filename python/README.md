ad3b0e9a3bccda63ca8c276bc484ffb1bbc0c2be
Maxime Brunet <max@brnt.mx>
2022-11-21 16:36:35 -0800

    feat(python): enable Perf map support

---
+++ python/README.md
@@ -0,0 +1,3 @@
+# Python
+
+See https://docs.python.org/3.12/howto/perf_profiling.html#perf-profiling
--- python/main.py
+++ python/main.py
@@ -1,10 +1,12 @@
-import time 
+import time
+
 
 def fibonacci(n):
     if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 while True:
     print(fibonacci(42))
