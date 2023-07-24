59c9e4f42c81178504f16b9f90a6ef2e2820bdd9
Kemal Akkoyun <kakkoyun@gmail.com>
2023-07-24 19:04:02 +0200

    Update python 3.12 perf support example
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- python/main.py
+++ python/main.py
@@ -1,13 +1,19 @@
-import time
-
-
 def fibonacci(n):
     if n == 1 or n == 2:
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+def foo(n):
+    for i in range(1, n):
+        print(fibonacci(i))
 
-while True:
-    print(fibonacci(42))
-    time.sleep(1)
+def bar(n):
+    foo(n)
+
+def baz(n):
+    bar(n)
+
+if __name__ == "__main__":
+    while True:
+        baz(42)
